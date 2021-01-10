fd1d3176ab64d6196c6af7908a0bc9b090560199
kbrv37 <apizano@purdue.edu>
2021-01-10 13:07:13 -0600

    refactor; add main method to class

--- src/chapters/three/stacks/threeinone/Stack.java
+++ src/chapters/three/stacks/threeinone/Stack.java
@@ -74,11 +74,27 @@ public class Stack<T> {
         return (T) array[index3];
     }
 
-    // Needs formal type parameter <T>
+    // Needs formal type parameter <T> for static method
     public static <T> void print(T val) {
         System.out.println(val);
     }
 
+    public static void main(String[] args) {
+        Stack<Integer> intStack = new Stack<>(2, 4 , 4);
+        intStack.push(1,2,3);
+        intStack.push(2,4,6);
 
+        Stack<String> strStack = new Stack<>(2, 4, 4);
+        strStack.push("1", "2", "3");
+        strStack.push("2","4","6");
 
+        System.out.println(intStack.popS1());
+        System.out.println(intStack.popS2());
+        System.out.println(intStack.popS3());
+
+        // testing static generic
+        //Stack.print(5);
+        //Stack.print(new String("HelloWorld"));
+
+    }
 }
