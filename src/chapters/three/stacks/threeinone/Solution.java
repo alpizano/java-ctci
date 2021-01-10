c57859659e4015a4bedfabfa0e0f1214a559a7c2
kbrv37 <apizano@purdue.edu>
2021-01-10 07:59:40 -0600

    ctci three in one stack problem

---
+++ src/chapters/three/stacks/threeinone/Solution.java
@@ -0,0 +1,8 @@
+package chapters.three.stacks.threeinone;
+
+// Describe how you could use a single array to implement three stacks
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+}
