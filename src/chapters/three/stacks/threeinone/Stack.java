04e90fa6dfa253343c0062f19bd64664800c60f8
kbrv37 <apizano@purdue.edu>
2021-01-10 08:53:05 -0600

    adding constructors

--- src/chapters/three/stacks/threeinone/Stack.java
+++ src/chapters/three/stacks/threeinone/Stack.java
@@ -1,24 +1,46 @@
 package chapters.three.stacks.threeinone;
 
 // Describe how you could use a single array to implement three stacks
-public class Solution {
+public class Stack {
     int[] array;
-    int size;
-    final int CAPACITY = 0;
+    int capacity1 = 0;
+    int capacity2 = 0;
+    int capacity3 = 0;
+    int size1;
+    int size2;
+    int size3;
+    int index1;
+    int index2;
+    int index3;
 
-    public Solution() {
-        array = new int[CAPACITY];
-        size = 0;
+    public Stack(int capacity1, int capacity2, int capacity3) {
+        array = new int[capacity1 + capacity2 + capacity3];
+        this.capacity1 = capacity1;
+        this.capacity2 = capacity2;
+        this.capacity3 = capacity3;
+        size1 = 0;
+        size2 = 0;
+        size3 = 0;
+        index1 = 0;
+        index2 = capacity1;
+        index3 = capacity1 + capacity2;
     }
 
-    public Solution(int capacity) {
-        array = new int[capacity];
-        size = 0;
+    public void push(int val1, int val2, int val3) {
+
+    }
+
+    public void pushS1(int val) {
+
+    }
+
+    public void pushS2(int val) {
+
+    }
+
+    public void pushS3(int val) {
+
     }
 
 
-    public static void main(String[] args) {
-
-
-    }
 }
