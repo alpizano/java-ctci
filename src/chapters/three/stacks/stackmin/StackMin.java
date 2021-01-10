191edee69daeab6ae23241b038678b989daad86e
kbrv37 <apizano@purdue.edu>
2021-01-10 13:28:52 -0600

    push/pop with O(1) time complexity; refactor package dirs

--- src/chapters/three/stacks/stackmin/StackMin.java
+++ src/chapters/three/stacks/stackmin/StackMin.java
@@ -1,5 +1,7 @@
-package chapters.three.stacks.threeinone;
+package chapters.three.stacks.stackmin;
 
+
+import java.util.EmptyStackException;
 
 // Design a stack which in addition to push and pop, has a function min
 // which returns the minimum element. Push, pop, and min should all operate in O(1) time.
@@ -11,9 +13,7 @@ public class StackMin<T> {
         Node<E> next;
 
         public Node() {
-
         }
-
 
         public Node(E val) {
             this.val=val;
@@ -38,15 +38,41 @@ public class StackMin<T> {
         return this;
     }
 
+    public T pop() {
+        if(head == null) {
+            throw new EmptyStackException();
+        }
+
+        Node<T> temp = head;
+        head = head.next;
+
+        // Clean up deleted node
+        temp.next = null;
+
+       return temp.val;
+    }
+
     public void print() {
         for(Node<T> cursor=head; cursor != null; cursor = cursor.next) {
             System.out.print(cursor.val + " ");
         }
+        System.out.println();
     }
 
     public static void main(String[] args) {
         StackMin<Integer> stackMin = new StackMin<>();
+        System.out.println(stackMin.head);
+        //System.out.println(stackMin.head.val); // NPE
+        //System.out.println(stackMin.head.next); // NPE
+
         stackMin.push(1).push(2).push(3).push(4).push(5);
+
+        //System.out.println(stackMin.head); // chapters.three.stacks.threeinone.StackMin$Node@1b6d3586
+        //System.out.println(stackMin.head.val);  // 5
+        //System.out.println(stackMin.head.next.val); // 4
+        stackMin.print();
+        System.out.println(stackMin.pop());
+        System.out.println(stackMin.pop());
         stackMin.print();
     }
 }
