82c94d7d5dc3f9ae254108a9f9489ea085f8ea3f
kbrv37 <apizano@purdue.edu>
2021-01-10 14:36:35 -0600

    fixed get min function to remember prev min values/states

--- src/chapters/three/stacks/stackmin/StackMin.java
+++ src/chapters/three/stacks/stackmin/StackMin.java
@@ -1,13 +1,16 @@
 package chapters.three.stacks.stackmin;
 
 
+import java.util.ArrayList;
 import java.util.EmptyStackException;
+import java.util.List;
 
 // Design a stack which in addition to push and pop, has a function min
 // which returns the minimum element. Push, pop, and min should all operate in O(1) time.
 public class StackMin<T> {
     private Node<T> head;
     private int min;
+    private List<Integer> mins;
 
     private static class Node<E> {
         private E val;
@@ -25,20 +28,26 @@ public class StackMin<T> {
     // default constructor
     public StackMin() {
         head = null;
-        min = 0;
+        mins = new ArrayList<>();
     }
 
     // constructor
     public StackMin(T val) {
         head = new Node<>(val);
-        min = (Integer) val;
+        //min = (Integer) val;
+        mins = new ArrayList<>((Integer)val);
     }
 
     public StackMin<T> push(T val) {
         Node<T> newNode = new Node<>(val);
 
-        if((Integer)val < min) {
-            min = (Integer) val;
+        if(head == null) {
+            //min = (Integer) val;
+            mins.add((Integer)val);
+        }
+        else if((Integer)val < mins.get(mins.size()-1)) {
+            //min = (Integer) val;
+            mins.add((Integer)val);
         }
 
         newNode.next = head;
@@ -53,6 +62,13 @@ public class StackMin<T> {
         }
 
         Node<T> temp = head;
+
+        int lastIndex = mins.size()-1;
+
+        if(temp.val == mins.get(lastIndex)) {
+            mins.remove(lastIndex);
+        }
+
         head = head.next;
 
         // Clean up deleted node
@@ -62,7 +78,9 @@ public class StackMin<T> {
     }
 
     public int min() {
-        return min;
+        int lastIndex = mins.size()-1;
+
+        return mins.get(lastIndex);
     }
 
     public void print() {
@@ -92,6 +110,20 @@ public class StackMin<T> {
         stackMin.print();
         System.out.println(stackMin.min());
 
-
+        System.out.println();
+        StackMin<Integer> ctciStack = new StackMin<>();
+        System.out.println("MIN TEST:");
+        System.out.println("pushed: " + ctciStack.push(5).head.val+ ", min: " + ctciStack.min());
+        ctciStack.print();
+        System.out.println("pushed: " + ctciStack.push(6).head.val+ ", min: " + ctciStack.min());
+        ctciStack.print();
+        System.out.println("pushed: " + ctciStack.push(3).head.val+ ", min: " + ctciStack.min());
+        ctciStack.print();
+        System.out.println("pushed: " + ctciStack.push(7).head.val+ ", min: " + ctciStack.min());
+        ctciStack.print();
+        System.out.println("popped value: " + ctciStack.pop()+", min: " + ctciStack.min());
+        ctciStack.print();
+        System.out.println("popped value: " + ctciStack.pop()+", min: " + ctciStack.min());
+        ctciStack.print();
     }
 }
