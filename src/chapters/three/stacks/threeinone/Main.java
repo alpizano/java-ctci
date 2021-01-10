a03afa15c2797b28000525eb4888d58aac6326a4
kbrv37 <apizano@purdue.edu>
2021-01-10 09:06:29 -0600

    added generic pop methods

--- src/chapters/three/stacks/threeinone/Main.java
+++ src/chapters/three/stacks/threeinone/Main.java
@@ -2,6 +2,11 @@ package chapters.three.stacks.threeinone;
 
 public class Main {
     public static void main(String[] args) {
-        Stack stack = new Stack(10, 20 ,30);
+        Stack<Integer> stack = new Stack<>(2, 4 ,4);
+        stack.push(1,2,3);
+        stack.push(2,4,6);
+
+
+
     }
 }
--- src/chapters/three/stacks/threeinone/Stack.java
+++ src/chapters/three/stacks/threeinone/Stack.java
@@ -1,8 +1,8 @@
 package chapters.three.stacks.threeinone;
 
 // Describe how you could use a single array to implement three stacks
-public class Stack {
-    int[] array;
+public class Stack<T> {
+    Object[] array;
     int capacity1 = 0;
     int capacity2 = 0;
     int capacity3 = 0;
@@ -14,7 +14,7 @@ public class Stack {
     int index3;
 
     public Stack(int capacity1, int capacity2, int capacity3) {
-        array = new int[capacity1 + capacity2 + capacity3];
+        array = new Object[capacity1 + capacity2 + capacity3];
         this.capacity1 = capacity1;
         this.capacity2 = capacity2;
         this.capacity3 = capacity3;
@@ -27,20 +27,52 @@ public class Stack {
     }
 
     public void push(int val1, int val2, int val3) {
+        pushS1(val1);
+        pushS2(val2);
+        pushS3(val3);
 
     }
 
     public void pushS1(int val) {
+        array[index1] = val;
+        size1++;
+        index1++;
 
     }
 
     public void pushS2(int val) {
-
+        array[index2] = val;
+        size2++;
+        index2++;
     }
 
     public void pushS3(int val) {
-
+        array[index3] = val;
+        size3++;
+        index3++;
     }
+
+    public T popS1() {
+        array[index1] = null;
+        size1--;
+        index1--;
+        return (T) array[index1];
+    }
+
+    public T popS2() {
+        array[index2] = null;
+        size2--;
+        index2--;
+        return (T) array[index2];
+    }
+
+    public T popS3() {
+        array[index3] = null;
+        size3--;
+        index3--;
+        return (T) array[index3];
+    }
+
 
 
 }
